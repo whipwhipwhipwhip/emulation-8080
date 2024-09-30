8feef61dc7f5b3ceda572ffa38199f6879b59248
Conor McMeel <conorm.ninetythree@gmail.com>
2024-09-30 20:49:51 +0100

    more disassembler stuff

--- dissasembler/dissasembler.c
+++ dissasembler/dissasembler.c
@@ -35,6 +35,7 @@ int Disassemble8080Op(unsigned char *codebuffer, int pc)
         case 0x0d: printf("DCR    C"); break;
         case 0x0e: printf("MVI    C,#$%02x", code[1]); opbytes=2; break;
         case 0x0f: printf("RRC"); break;
+
         case 0x10: printf("NOP"); break;
         case 0x11: printf("LXI    D,#$%02x%02x", code[2], code[1]); opbytes=3; break;    
         case 0x12: printf("STAX   D"); break;    
@@ -51,6 +52,7 @@ int Disassemble8080Op(unsigned char *codebuffer, int pc)
         case 0x1d: printf("DCR    E"); break;
         case 0x1e: printf("MVI    E,#$%02x", code[1]); opbytes=2; break;
         case 0x1f: printf("RAR"); break;   
+
         case 0x20: printf("NOP"); break;
         case 0x21: printf("LXI    H,#$%02x%02x", code[2], code[1]); opbytes=3; break;    
         case 0x22: printf("SHLD   #$%02x%02x", code[2], code[1]); opbytes=3; break;    
@@ -67,8 +69,75 @@ int Disassemble8080Op(unsigned char *codebuffer, int pc)
         case 0x2d: printf("DCR    L"); break;
         case 0x2e: printf("MVI    L,#$%02x", code[1]); opbytes=2; break;
         case 0x2f: printf("CMA"); break;    
-        case 0x3e: printf("MVI    A,#0x%02x", code[1]); opbytes = 2; break;    
-        /* ........ */    
+
+	case 0x30: printf("NOP"); break;
+	case 0x31: printf("LXI	  SP,#$%02x%02x", code[2], code[1]); opbytes=3; break;
+	case 0x32: printf("STA    #$%02x%02x", code[2], code[1]); opbytes=3; break;
+	case 0x33: printf("INX    SP"); break;
+	case 0x34: printf("INR    M"); break;
+	case 0x35: printf("DCR    M"); break;
+	case 0x36: printf("MVI    M, #$%02x", code[1]); opbytes = 2; break;
+	case 0x37: printf("STC"); break;
+	case 0x38: printf("NOP"); break;
+	case 0x39: printf("DAD    SP"); break;
+	case 0x3a: printf("LDA    #$%02x%02x", code[2], code[1]); opbytes=3; break;
+	case 0x3b: printf("DCX    SP"); break;
+	case 0x3c: printf("INR    A"); break;
+	case 0x3d: printf("DCR    A"); break;
+        case 0x3e: printf("MVI    A,#0x%02x", code[1]); opbytes = 2; break;
+	case 0x3f: printf("CMC") break;
+
+	case 0x40: printf("MOV    B,B"); break;
+	case 0x41: printf("MOV    B,C"); break;
+	case 0x42: printf("MOV    B,D"); break;
+	case 0x43: printf("MOV    B,E"); break;
+	case 0x44: printf("MOV    B,H"); break;
+	case 0x45: printf("MOV    B,L"); break;
+	case 0x46: printf("MOV    B,M"); break;
+	case 0x47: printf("MOV    B,A"); break;
+	case 0x48: printf("MOV    C,B"); break;
+	case 0x49: printf("MOV    C,C"); break;
+	case 0x4a: printf("MOV    C,D"); break;
+	case 0x4b: printf("MOV    C,E"); break;
+	case 0x4c: printf("MOV    C,H"); break;
+	case 0x4d: printf("MOV    C,L"); break;
+	case 0x4e: printf("MOV    C,M"); break;
+	case 0x4f: printf("MOV    C,A"); break;
+
+	case 0x50: printf("MOV    D,B"); break;
+	case 0x51: printf("MOV    D,C"); break;
+	case 0x52: printf("MOV    D,D"); break;
+	case 0x53: printf("MOV    D,E"); break;
+	case 0x54: printf("MOV    D,H"); break;
+	case 0x55: printf("MOV    D,L"); break;
+	case 0x56: printf("MOV    D,M"); break;
+	case 0x57: printf("MOV    D,A"); break;
+	case 0x58: printf("MOV    E,B"); break;
+	case 0x59: printf("MOV    E,C"); break;
+	case 0x5a: printf("MOV    E,D"); break;
+	case 0x5b: printf("MOV    E,E"); break;
+	case 0x5c: printf("MOV    E,H"); break;
+	case 0x5d: printf("MOV    E,L"); break;
+	case 0x5e: printf("MOV    E,M"); break;
+
+	case 0x60: printf("MOV    H,B"); break;
+	case 0x61: printf("MOV    H,C"); break;
+	case 0x62: printf("MOV    H,D"); break;
+	case 0x63: printf("MOV    H,E"); break;
+	case 0x64: printf("MOV    H,H"); break;
+	case 0x65: printf("MOV    H,L"); break;
+	case 0x66: printf("MOV    H,M"); break;
+	case 0x67: printf("MOV    H,A"); break;
+	case 0x68: printf("MOV    L,B"); break;
+	case 0x69: printf("MOV    L,C"); break;
+	case 0x6a: printf("MOV    L,D"); break;
+	case 0x6b: printf("MOV    L,E"); break;
+	case 0x6c: printf("MOV    L,H"); break;
+	case 0x6d: printf("MOV    L,L"); break;
+	case 0x6e: printf("MOV    L,M"); break;
+	case 0x6f: printf("MOV    L,A"); break;
+
+        /* ........ */       
         case 0xc3: printf("JMP    $%02x%02x",code[2],code[1]); opbytes = 3; break;    
         /* ........ */    
     }    
